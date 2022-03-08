86f7895129f811416e56ab9a60b7a4773f411b22
Your Name <comptonaustin82@gmail.com>
2022-03-08 10:46:13 -0800

    cors added

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(function(req, res, next){
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
+app.use(cors())
 
 let projects = []
 let solanaLinks = []
